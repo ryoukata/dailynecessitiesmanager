ba99bd1dae01ceb2ca3aa2a363ecaff7a24b9ae9
ryoukata <ryoya3091@gmail.com>
2020-09-17 00:10:29 +0900

    first commit

---
+++ src/test/java/com/ryoya/dailynecessitiesmanager/DailynecessitiesmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryoya.dailynecessitiesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailynecessitiesmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
